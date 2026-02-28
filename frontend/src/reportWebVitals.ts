c266357f401048ef9d14c2b9182d3c9c648ce8c2
firstsmile-dev <firstsmile1001@gmail.com>
2026-02-28 22:28:22 +0900

    fix: package.json

--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,14 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onFCP, onINP, onLCP, onTTFB } from 'web-vitals';
+
+type Metric = { name: string; value: number; id: string; delta: number };
+type ReportHandler = (metric: Metric) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    onCLS(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onINP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
